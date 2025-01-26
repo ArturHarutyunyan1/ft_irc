40b97e91ab8c27db0648fe4e1f7719599036051a
= <=>
2025-01-26 12:50:34 +0400

    PASS command

--- src/Requests.cpp
+++ src/Requests.cpp
@@ -8,26 +8,27 @@ void Requests::handleRequest()
 {
     std::string message(_message);
     std::string response;
+    std::string value;
+    size_t pos = message.find(' ');
+    
+    if (pos != std::string::npos)
+        value = message.substr(pos + 1);
+    size_t end = value.size();
+    while (end > 0 && (value[end - 1] == ' ' || value[end - 1] == '\n' || value[end - 1] == '\r'))
+        --end;
+    value = value.substr(0, end);
     if (message == "HELP\n")
         response = helpMessage();
     else if (std::strncmp(_message, "PASS", 4) == 0)
-        response = PASS(message);
+        response = PASS(value);
+    else
+        response = "Unknown command\n";
     send(this->_fds[this->_fd].fd, response.c_str(), response.size(), 0);
 }
 
 std::string Requests::PASS(std::string msg)
 {
-    std::string pass;
-    size_t pos = msg.find(' ');
-    
-    if (pos != std::string::npos)
-        pass = msg.substr(pos + 1);
-    size_t end = pass.size();
-    while (end > 0 && (pass[end - 1] == ' ' || pass[end - 1] == '\n' || pass[end - 1] == '\r'))
-        --end;
-    pass = pass.substr(0, end);
-    
-    if (pass == _password && !_isSet)
+    if (msg == _password && !_isSet)
     {
         _isSet = true;
         return ("Password was set successfully!\n");
